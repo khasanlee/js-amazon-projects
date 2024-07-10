03c299f3fc93850298353ef952759206af63aa11
Khasan <xasaniskandarov2000@gmail.com>
2024-07-10 18:21:08 +0900

    updated for 14k

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -39,7 +39,7 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
                     Update
                   </span>
-                  <input class="quantity-input">
+                  <input class="quantity-input js-quantity-input-${matchingProduct.id}">
                   <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
                   <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
@@ -122,6 +122,9 @@ document.querySelectorAll('.js-save-quantity-link').forEach((link)=> {
         const productId = link.dataset.productId
         const container = document.querySelector(`.js-cart-item-container-${productId}`)
         container.classList.remove('is-editing-quantity')
+        const quantityInput = document.querySelector(`.js-quantity-selector-input-${productId}`)
+        const newQuantity = Number(quantityInput)
+
     })
 })
 
